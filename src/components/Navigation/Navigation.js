6da2f1dcc000cd8763bb1ef4cef32ecacfca4c08
Kacper Kosicki <kosickikacper1@gmail.com>
2024-09-14 15:08:35 +0200

    Edit Navigation and Home

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -9,12 +9,16 @@ const Navigation = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
+
   return (
     <nav className={styles.navContainer}>
       <div className={styles.navLogo}>
         <img src="/assets/images/orzel_peckowo.png" alt="Logo" />
       </div>
-      
+
       <div className={styles.hamburger} onClick={toggleMenu}>
         <div className={styles.hamburgerBar}></div>
         <div className={styles.hamburgerBar}></div>
@@ -22,16 +26,30 @@ const Navigation = () => {
       </div>
 
       <div className={`${styles.navLinks} ${isMenuOpen ? styles.showMenu : ''}`}>
-        <Link to="/" className={styles.link}>Strona Główna</Link>
-        <Link to="/mecze" className={styles.link}>Mecze</Link>
-        <Link to="/druzyny" className={styles.link}>Drużyny</Link>
-        <Link to="/tabela" className={styles.link}>Tabela</Link>
-        <Link to="/zawodnicy" className={styles.link}>Zawodnicy</Link>
-        <Link to="/o-klubie" className={styles.link}>O Klubie</Link>
-        <Link to="/kontakt" className={styles.link}>Kontakt</Link>
+        <Link to="/" className={styles.link} onClick={closeMenu}>
+          Strona Główna
+        </Link>
+        <Link to="/mecze" className={styles.link} onClick={closeMenu}>
+          Mecze
+        </Link>
+        <Link to="/druzyny" className={styles.link} onClick={closeMenu}>
+          Drużyny
+        </Link>
+        <Link to="/tabela" className={styles.link} onClick={closeMenu}>
+          Tabela
+        </Link>
+        <Link to="/zawodnicy" className={styles.link} onClick={closeMenu}>
+          Zawodnicy
+        </Link>
+        <Link to="/o-klubie" className={styles.link} onClick={closeMenu}>
+          O Klubie
+        </Link>
+        <Link to="/kontakt" className={styles.link} onClick={closeMenu}>
+          Kontakt
+        </Link>
       </div>
     </nav>
   );
-}
+};
 
 export default Navigation;
